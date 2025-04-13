b2d3f5b4123894d669c698b7d5eb1793a012ac98
rahimuj570 <rahimujjaman2002@gmail.com>
2025-04-13 15:42:08 +0600

    dummy gettodo middleware is working

--- main.go
+++ main.go
@@ -9,11 +9,20 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	getTodo_V := http.HandlerFunc(getTodo)
+	getTodoMW_V := getTodoMW(getTodo_V)
+
 	mux.HandleFunc("GET /", test)
+	mux.HandleFunc("GET /get", getTodoMW_V.ServeHTTP)
 
 	//run server
 	err := http.ListenAndServe(":8000", mux)
 	log.Fatal((err))
+}
+
+// to get
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Todo is getted")
 }
 
 // to test
@@ -21,7 +30,7 @@ func test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "API WORKING Middleware")
 }
 
-// testing middleware dor gettodo
+// testing middleware for gettodo
 func getTodoMW(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("MW WORKING")
